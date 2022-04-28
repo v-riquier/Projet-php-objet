b8a6e407de5ddc484f5a7ba631077cc57eb82c06
v-riquier <vincent.riquier80@orange.fr>
2022-04-28 15:09:12 +0200

    change

--- PHP/modifcomm.php
+++ PHP/modifcomm.php
@@ -1,29 +1,29 @@
 <?php
 include "header.php";
 echo "<h1>Modification de commentaire</h1>";
-/*if (isset($_POST["btnComm"])) {
-        if (isset($_SESSION["NoUser"])) {
-            $Req = $MaBase->query("UPDATE Commentaire SET Commentaire = '" . $_POST["txtComm"] . "' WHERE IdAstuce = '" . $_POST["nbastuce"] . "'");
-        } else echo "Vous n'êtes pas connecté";
-        echo '<p><a href="modifastuce.php">Modifier un autre commentaire</a></p>';
-    } else {*/
+if (isset($_POST["btnComm"])) {
+    if (isset($_SESSION["NoUser"])) {
+        $Req = $MaBase->query("UPDATE Commentaire SET Commentaire = '" . $_POST["txtComm"] . "' WHERE IdComm = '" . $_POST["nbComm"] . "'");
+        echo "Commentaire modifié";
+    } else echo "Vous n'êtes pas connecté";
+    echo '<p><a href="modifcomm.php">Modifier un autre commentaire</a></p>';
+} else {
 ?>
-<form action="" method="post">
-    Commentaire : <select name="nbastuce" id="nbastuce" required>
-        <option value="">---Choisir Commentaire---</option>
-        <?php $reponse = $MaBase->query("SELECT Jeux.Titre,Astuce.IdAstuce,Astuce.Astuce FROM Jeux,Astuce WHERE Astuce.IdJeux=Jeux.IdJeux ORDER BY Jeux.Titre;");
-        while ($donnees = $reponse->fetch()) {
-        ?>
-            <option value="<?php echo $donnees['IdAstuce']; ?>">
-                <?php echo $donnees["Titre"] . " : " . $donnees['Astuce']; ?>
-            </option>
-        <?php } ?>
-    </select>
-    Modif : <input type="text" name="txtComm" id="txtComm" required>
-    <input type="submit" name="btnComm" value="Modifier">
-</form>
-<span>Le bouton ne marche pas</span>
+    <form action="" method="post">
+        Commentaire : <select name="nbComm" id="nbComm" required>
+            <option value="">---Choisir Commentaire---</option>
+            <?php $reponse = $MaBase->query("SELECT Jeux.Titre,Commentaire.IdComm,Astuce.Astuce,Commentaire.Commentaire FROM Jeux,Astuce,Commentaire WHERE Astuce.IdJeux=Jeux.IdJeux AND Commentaire.IdAstuce = Astuce.IdAstuce ORDER BY Jeux.Titre");
+            while ($donnees = $reponse->fetch()) {
+            ?>
+                <option value="<?php echo $donnees['IdComm']; ?>">
+                    <?php echo $donnees["Titre"] . " : " . $donnees["Astuce"] . " : " . $donnees["Commentaire"]; ?>
+                </option>
+            <?php } ?>
+        </select>
+        Modif : <input type="text" name="txtComm" id="txtComm" required>
+        <input type="submit" name="btnComm" value="Modification">
+    </form>
 <?php
-//}
+}
 include "footer.php";
 ?>
